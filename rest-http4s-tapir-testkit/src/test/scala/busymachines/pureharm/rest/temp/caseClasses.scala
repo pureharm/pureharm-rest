2f38aab4ce166912a41af5f71f6ea043ce60a114
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-04-05 13:33:27 +0300

    Remove unused ShowThrowable

--- rest-http4s-tapir-testkit/src/test/scala/busymachines/pureharm/rest/temp/caseClasses.scala
+++ rest-http4s-tapir-testkit/src/test/scala/busymachines/pureharm/rest/temp/caseClasses.scala
@@ -19,7 +19,6 @@ package busymachines.pureharm.rest.temp
 import busymachines.pureharm.rest._
 import busymachines.pureharm.json._
 import busymachines.pureharm.json.implicits._
-import ShowThrowable._
 
 /** @author Lorand Szakacs, https://github.com/lorandszakacs
   * @since 10 Jul 2020
